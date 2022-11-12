897ae13e00b525687b738376261cf737f154b9bf
Colin Duggan <duggan.colin@gmail.com>
2022-11-12 22:18:04 +0000

    update

--- internal/pkg/cmd/billing/get.go
+++ internal/pkg/cmd/billing/get.go
@@ -2,17 +2,17 @@ package billing
 
 import (
 	"fmt"
-	billingapi "github.com/cduggn/cloudcost/internal/pkg/billing"
+	"github.com/cduggn/cloudcost/internal/pkg/billing"
 	"github.com/spf13/cobra"
 )
 
 func GetBillingSummary(cmd *cobra.Command, args []string) {
 	req := NewCostAndUsageRequest(cmd)
-	report := billingapi.GetAWSCostAndUsage(req)
+	report := billing.GetAWSCostAndUsage(req)
 	report.Print()
 }
 
-func NewCostAndUsageRequest(cmd *cobra.Command) billingapi.CostAndUsageRequest {
+func NewCostAndUsageRequest(cmd *cobra.Command) billing.CostAndUsageRequest {
 
 	dimensions, err := cmd.Flags().GetStringSlice("group-by-dimension")
 	if err != nil {
@@ -21,11 +21,11 @@ func NewCostAndUsageRequest(cmd *cobra.Command) billingapi.CostAndUsageRequest {
 
 	filterBy, _ := cmd.Flags().GetString("filter-by")
 
-	return billingapi.CostAndUsageRequest{
+	return billing.CostAndUsageRequest{
 		Granularity: cmd.Flags().Lookup("granularity").Value.String(),
 		GroupBy:     dimensions,
 		Tag:         cmd.Flags().Lookup("group-by-tag").Value.String(),
-		Time: billingapi.Time{
+		Time: billing.Time{
 			Start: cmd.Flags().Lookup("start-date").Value.String(),
 			End:   cmd.Flags().Lookup("end-date").Value.String(),
 		},
