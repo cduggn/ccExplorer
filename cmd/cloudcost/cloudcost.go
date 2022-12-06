6a944d6b6fc455a2c8e5cc3425372b6be7290f51
Colin Duggan <duggan.colin@gmail.com>
2022-12-06 22:03:43 +0000

    Basic setup for db creation

--- cmd/cloudcost/cloudcost.go
+++ cmd/cloudcost/cloudcost.go
@@ -1,7 +1,82 @@
 package main
 
-import "github.com/cduggn/cloudcost/internal/pkg/cmd"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
+)
 
 func main() {
-	cmd.Execute()
+	//cmd.Execute()
+	createDB()
+	insertCustomer()
+}
+
+func createDB() {
+
+	if _, err := os.Stat("cloudcost.db"); os.IsNotExist(err) {
+		db, err := os.Create("cloudcost.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		createTable()
+
+		defer db.Close()
+
+		log.Println("Database created")
+	}
+
+}
+
+func createTable() {
+	db, err := sql.Open("sqlite3", "./cloudcost.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+		CREATE TABLE customers (
+		    till_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+		    client_id VARCHAR(64) NULL, 
+		    first_name VARCHAR(255) NOT NULL, 
+		    last_name VARCHAR(255) NOT NULL, 
+		    guid VARCHAR(255) NULL, 
+		    dob DATETIME NULL, type VARCHAR(1))
+		    )
+		    )
+		`
+
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s", err, sqlStmt)
+		return
+	}
+}
+
+func insertCustomer() {
+	db, err := sql.Open("sqlite3", "./cloudcost.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("INSERT INTO customers(client_id, first_name, last_name, guid, dob, type) values(?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec("1", "John", "Doe", "123456789", "1980-01-01", "C")
+	if err != nil {
+		log.Fatal(err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(id)
 }
--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/aws/smithy-go v1.13.4 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.1.0 // indirect
