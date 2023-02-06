8756ae901928c1f1698b88ed6728ffe706eee84d
Colin Duggan <duggan.colin@gmail.com>
2023-02-06 12:17:43 +0000

    Testing github secret scanning

--- cmd/ccexplorer/ccexplorer.go
+++ cmd/ccexplorer/ccexplorer.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/cduggn/ccexplorer/internal/commands"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	commands.Execute()
+
+	// testing github secrete scanner
+	aws_access_key_id := "YIYIOUY£465465465hjkhjk"
+	aws_secret_access_key := "YIYIOUY£465465465098908908"
+	fmt.Print(aws_access_key_id, aws_secret_access_key)
 }
