d5ccbb3c54b68dbbefcfe205ffff08c47ad0500e
Colin Duggan <duggan.colin@gmail.com>
2023-02-16 21:35:05 +0000

    Add viper config and flag for openai api key

--- cmd/ccexplorer/ccexplorer.go
+++ cmd/ccexplorer/ccexplorer.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/cduggn/ccexplorer/internal/commands"
 	_ "github.com/mattn/go-sqlite3"
+	"os"
 )
 
 func main() {
-	commands.Execute()
+	root := commands.RootCommand()
+
+	if err := root.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(126)
+	}
+
 }
--- internal/commands/root.go
+++ internal/commands/root.go
@@ -1,12 +1,13 @@
 package commands
 
 import (
-	"github.com/cduggn/ccexplorer/internal/commands/aws-presets"
+	"fmt"
+	aws_presets "github.com/cduggn/ccexplorer/internal/commands/aws-presets"
 	"github.com/cduggn/ccexplorer/internal/commands/get"
 	"github.com/cduggn/ccexplorer/pkg/logger"
 	"github.com/common-nighthawk/go-figure"
 	"github.com/spf13/cobra"
-	"os"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -15,26 +16,46 @@ var (
 		Short: "A CLI tool to explore cloud costs and usage",
 		Long:  paintRootHeader(),
 	}
+	cfgFile string
 )
 
-func init() {
-	rootCmd.AddCommand(get.AWSCostAndUsageCommand())
-	rootCmd.AddCommand(aws_presets.AddAWSPresetCommands())
-}
-
-func Execute() {
+func RootCommand() *cobra.Command {
 	_, err := logger.New()
 	if err != nil {
 		panic(err.Error())
 	}
+	cobra.OnInitialize(initConfig)
+	return rootCmd
+}
 
-	err = rootCmd.Execute()
+func init() {
+	rootCmd.AddCommand(get.AWSCostAndUsageCommand())
+	rootCmd.AddCommand(aws_presets.AddAWSPresetCommands())
+	err := viper.BindPFlag("open_ai_api_key", rootCmd.PersistentFlags().Lookup(
+		"OPEN_AI_API_KEY"))
 	if err != nil {
-		os.Exit(126)
+		fmt.Println("OPEN_AI_API_KEY not set")
 	}
 }
 
 func paintRootHeader() string {
 	myFigure := figure.NewFigure("ccExplorer", "thin", true)
 	return myFigure.String()
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath(".")
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".cobra")
+	}
+
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Println("No config file found:", err.Error())
+	}
 }
