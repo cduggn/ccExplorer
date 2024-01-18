5330c05d6613236961ea784756c67ef43d77ba63
cduggn <duggan.colin@gmail.com>
2024-01-18 23:09:51 +0000

    v0.7.2: upgrade external dependencies aws-sdk-v2, go-openai, go-echarts

--- internal/core/handlers/commandline/root.go
+++ internal/core/handlers/commandline/root.go
@@ -11,7 +11,7 @@ import (
 var (
 	rootCmd = &cobra.Command{
 		Use:     "ccexplorer",
-		Version: "0.7.1",
+		Version: "0.7.2",
 		Short:   "A CLI tool to explore cloud costs and usage",
 		Long:    paintRootHeader(),
 	}
